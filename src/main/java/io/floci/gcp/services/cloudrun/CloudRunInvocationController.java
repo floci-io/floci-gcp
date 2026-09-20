357e7d2fcd0df0e3f5305a6ef8e02bb84d535146
Martín Heras <mheras@gmail.com>
2026-09-20 00:11:16 +0000

    Revert "fix(cloudrun): proxy runtimes over HTTP/1.1"
    This reverts commit 24e25dad352e28e32c825f3e78a55938977dc1bd.

--- src/main/java/io/floci/gcp/services/cloudrun/CloudRunInvocationController.java
+++ src/main/java/io/floci/gcp/services/cloudrun/CloudRunInvocationController.java
@@ -62,7 +62,6 @@ public class CloudRunInvocationController {
     public CloudRunInvocationController(CloudRunService cloudRunService) {
         this.cloudRunService = cloudRunService;
         this.httpClient = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
                 .followRedirects(HttpClient.Redirect.NEVER)
                 .build();
     }
